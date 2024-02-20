819b22a4b48638c3e991b297c9a7d8f7dd9ce8b2
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-20 20:15:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let frm = document.querySelector('#form1');
-        
-        frm.addEventListener('submit',(event) =>{
-            event.preventDefault();                     //prevent default action
-            let frmdata = new FormData(frm);            //place all form data into variable frmdata.
-            let obj = Object.fromEntries(frmdata);      //create object of variable 'frmdata'.     
-            
-            let txt ='';
-            for (let x in obj) {
-                 txt = txt + `${obj[x]} `;
-            };
-            window.alert(txt);  
-        });
+
+let frm = document.querySelector('#form1');        
+	frm.addEventListener('submit',(event) =>{
+		event.preventDefault();                     //prevent default action
+		let frmdata = new FormData(frm);            //place all form data into variable frmdata.
+		let obj = Object.fromEntries(frmdata);      //create object of variable 'frmdata'.     
+		
+		let txt ='';
+		for (let x in obj) {
+			 txt = txt + `${obj[x]} `;
+		};
+		window.alert(txt);  
+});
