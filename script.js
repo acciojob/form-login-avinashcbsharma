c1860d7ac79c567f178457b06884b22d01502aa4
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-20 19:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
-
-}
+let frm = document.querySelector('#form1');
+        
+        frm.addEventListener('submit',(event) =>{
+            event.preventDefault();                     //prevent default action
+            let frmdata = new FormData(frm);            //place all form data into variable frmdata.
+            let obj = Object.fromEntries(frmdata);      //create object of variable 'frmdata'.     
+            
+            let txt ='';
+            for (let x in obj) {
+                 txt = txt + `${obj[x]} `;
+            };
+            window.alert(txt);  
+        });
