116494866c1abc19d293e6db0d9347db1ace6b3e
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-20 21:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 
-// let frm = document.querySelector('#form1');        
-// 	frm.addEventListener('submit',(event) =>{
-// 		event.preventDefault();                     //prevent default action
-// 		let frmdata = new FormData(frm);            //place all form data into variable frmdata.
-// 		let obj = Object.fromEntries(frmdata);      //create object of variable 'frmdata'.     
-		
-// 		let txt ='';
-// 		for (let x in obj) {
-// 			 txt = txt + `${obj[x]} `;
-// 		};
-// 		window.alert(txt);  
-// });
+let frm = document.querySelector('#form1');        
 function getFormValues() {
-    // Prevent the default form submission
-    event.preventDefault();
+	 event.preventDefault();
 
     // Get the values from the form
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
 
     // Display the values using alert
-    alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    alert( firstName + ' ' +lastName);
 }
 
 // Attach the function to the form submission event
-document.getElementById("myForm").addEventListener("submit", getFormValues);
+frm.addEventListener("submit", getFormValues);
